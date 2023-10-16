06d04c57c4b1582d3fa1fd8d02c0a971e199ad24
Zhi Yao <zhiyao0223@gmail.com>
2023-10-16 23:50:28 +0800

    [Update] Update image domains to be accepted for the website, temporary use before downloading image into local

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // Temporary use this, remove later
+    domains: ["images4.alphacoders.com", "assets.gamepur.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
