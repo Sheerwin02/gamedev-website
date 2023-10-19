c5830c54a0344e24d1b34485976c58f70070d03a
Sheerwin <sheerwin2002@hotmail.com>
2023-10-19 21:55:28 +0800

    [Update] Add a ModelBase and optimize model and repository

--- src/app/repositories/RecruitmentRepository.ts
+++ src/app/repositories/RecruitmentRepository.ts
@@ -1,16 +1,22 @@
-import DbHelper from '../db/DbHelper';
-import { Recruitment, defineRecruitmentModel } from '../models/Recruitment';
+import { Recruitment } from '../models/Recruitment';
 import RepositoryBase from '../base/RepositoryBase';
+import DbHelper from '../db/DbHelper';
 
 class RecruitmentRepository extends RepositoryBase<Recruitment> {
-  static instance: RecruitmentRepository;
-  constructor() {
-    super(defineRecruitmentModel(DbHelper.getInstance().getSequelize()));
+  private static instance: RecruitmentRepository | null = null;
+
+  private constructor(model: typeof Recruitment) {
+    super(model);
   }
 
   static getInstance(): RecruitmentRepository {
     if (!RecruitmentRepository.instance) {
-      RecruitmentRepository.instance = new RecruitmentRepository();
+      const sequelize = DbHelper.getInstance().getSequelize();
+
+      // Initialize the Recruitment model
+      const model = Recruitment.initialize(sequelize) as typeof Recruitment;
+
+      RecruitmentRepository.instance = new RecruitmentRepository(model);
     }
     return RecruitmentRepository.instance;
   }
