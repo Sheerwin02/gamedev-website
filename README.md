1afaeb09697e767ec4413f9f7b4f346f4d501e55
Sheerwin <sheerwin2002@hotmail.com>
2023-11-18 13:30:38 +0800

    [Update] Refactor file upload component and recruitment item

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+# Hana Studio Website
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+  - [File Upload Component](#file-upload-component)
+- [Development](#development)
+- [Contributing](#contributing)
+- [License](#license)
+
 ## Getting Started
 
-Hana Studio Website
+### Hana Studio Website
+
+Welcome to the Hana Studio website! This is a website of a long-term project - Project Hana by Gan Sheer Win, Ng Zhi Yao, Lee Khoon Fang, Goh Song Jing, Bryan Yeoh and Tan Yong Sheng.
+
+#### File Upload Component
+
+The file upload component is used for uploading resumes in the recruitment section. By default, the upload functionality is temporarily disabled, and a message is displayed indicating that the feature is currently unavailable.
+
+##### How to Enable Upload Functionality
+
+1. Open the `FileUploadComponent.tsx` file located in the `src/components/file` directory.
+
+2. Locate the `isDisabled` state variable at the beginning of the component:
+
+   ```tsx
+   const [isDisabled, setIsDisabled] = useState(true);
--- components/recruitment/RecruitmentItem.tsx
+++ components/recruitment/RecruitmentItem.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 interface Recruitment {
   id: number;
@@ -10,17 +11,45 @@ interface Recruitment {
 
 interface Props {
   recruitment: Recruitment;
+  onItemClicked: (recruitment: Recruitment) => void;
 }
 
-const RecruitmentItem: React.FC<Props> = ({ recruitment }) => {
+const RecruitmentItem: React.FC<Props> = ({ recruitment, onItemClicked }) => {
+  if (!recruitment || !recruitment.position) {
+    return null;
+  }
+
+  const [isSaved, setIsSaved] = useState(false);
+
+  const handleSaveToggle = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Prevents the click event from reaching the parent div
+    setIsSaved((prev) => !prev);
+    // Optionally, you can add logic to save the state to your backend
+  };
+
   return (
-    <div className="rounded-lg bg-gray-800 border border-gray-600 shadow-md p-4 my-4 cursor-pointer hover:bg-gray-700 transition-transform transform hover:scale-105">
-      <h3 className="text-lg font-semibold mb-2 text-blue-400">
-        {recruitment.position}
-      </h3>
-      <p className="text-gray-300 mb-2">{recruitment.description}</p>
-      <p className="text-gray-300 mb-2">{recruitment.requirements}</p>
-      <p className="text-gray-400">Posted Date: {recruitment.postedDate}</p>
+    <div
+      className="rounded-lg bg-white border border-gray-300 shadow-md p-4 my-4 transition-transform transform hover:scale-105 cursor-pointer"
+      onClick={() => onItemClicked(recruitment)}
+    >
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="text-lg font-semibold text-indigo-600">
+          {recruitment.position}
+        </h3>
+        <button
+          className={`text-${isSaved ? "pink" : "gray"}-500 hover:text-${
+            isSaved ? "pink" : "gray"
+          }-600 transition-colors`}
+          onClick={handleSaveToggle}
+        >
+          {isSaved ? <FaHeart /> : <FaRegHeart />}
+        </button>
+      </div>
+      <p className="text-gray-700 mb-2">{recruitment.description}</p>
+      <div className="flex justify-between items-center">
+        <p className="text-gray-700 mb-2">{recruitment.requirements}</p>
+        <p className="text-gray-500">Posted Date: {recruitment.postedDate}</p>
+      </div>
     </div>
   );
 };
