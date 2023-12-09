03b979daebdf9ddfbb4e072bd010eb1b4a910178
Sheerwin <sheerwin2002@hotmail.com>
2023-12-09 12:04:21 +0800

    [Update] Minor bug fixes and layout changes in Recruitment

--- components/recruitment/RecruitmentDetailsPopUp.tsx
+++ components/recruitment/RecruitmentDetailsPopUp.tsx
@@ -8,7 +8,7 @@ interface Recruitment {
   id: number;
   position: string;
   description: string;
-  requirements: string;
+  requirement: string;
   postedDate: string;
 }
 
@@ -90,7 +90,7 @@ const RecruitmentDetailsPopup: React.FC<Props> = ({ recruitment, onClose }) => {
                 {recruitment?.position}
               </h3>
               <p className="text-lg mb-2">{recruitment?.description}</p>
-              <p className="text-lg mb-2">{recruitment?.requirements}</p>
+              <p className="text-lg mb-2">{recruitment?.requirement}</p>
               <p className="mb-4">Posted Date: {recruitment?.postedDate}</p>
             </div>
 
